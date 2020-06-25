110710e071f4037b2447c8eafec0deb02a2fcf8f
Your Name <MochaJavaScrpt@gmail.com>
2020-06-25 16:45:08 -0500

    update server address

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./Controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-tapered-66387',
     user : 'postgres',
     password : 'Tillerwills733',
     database : 'smartbrain'
